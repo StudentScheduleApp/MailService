0cb6dedf1b062fdc9b3bf7aa4b22192b37d51dee
IlyaKrn <ilyakornoukhov@gmail.com>
2023-11-21 23:46:51 +0300

    bugfix security

--- src/main/java/com/studentscheduleapp/mailservice/services/AuthorizeServiceService.java
+++ src/main/java/com/studentscheduleapp/mailservice/services/AuthorizeServiceService.java
@@ -1,16 +1,15 @@
 package com.studentscheduleapp.mailservice.services;
 
-import com.studentscheduleapp.mailservice.repos.ServiceTokenRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorizeServiceService {
-    @Autowired
-    private ServiceTokenRepository serviceTokenRepository;
+    @Value("${service.token}")
+    private String serviceToken;
 
-    public boolean authorize(String token) throws Exception {
-        return serviceTokenRepository.existsByServiceToken(token);
+    public boolean authorize(String token){
+        return serviceToken.equals(token);
     }
-
 }
